a5a08d150a01f47acbf0484ff70b86d6c0f7bbf8
mongmeo-dev <mongmeo.dev@gmail.com>
2022-10-08 16:46:45 +0900

    RP-9 불필요한 @Setter 어노테이션 제거 완료

--- src/main/java/dev/teamkjk/reservation_system/persistence/entity/performance/PerformanceEntity.java
+++ src/main/java/dev/teamkjk/reservation_system/persistence/entity/performance/PerformanceEntity.java
@@ -11,10 +11,8 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
